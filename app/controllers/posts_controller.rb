aa53dfc65fe6cd0c93a961b23992dc08b9f39df5
Sean m. Craig <sean.m.craig02@gmail.com>
2015-09-27 00:51:47 -0600

    added post CRUDability

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post=Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "It Worked!"
+		else
+			render 'new', notice:"Something Went Wrong!"
+		end
+	end
+		
+	def show
+	end
+	def edit
+	end
+
+	def update
+
+		if @post.update post_params
+			redirect_to @post, notice: "It was saved!"
+		else
+			render 'edit'
+		end
+	end
+		def destroy
+		    @post.destroy(params[:id])
+		    @post.destroy
+
+		    redirect_to action: :index
+		end
+	
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+	
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+end
